d80aa87a5829f9f7ad38d31edb9ff699cf55eb62
Isa0-07 <7ortizisaias@gmail.com>
2023-05-31 19:20:25 -0300

    proyecto con login

---
+++ src/main/java/uaa/tif/Tif.java
@@ -0,0 +1,22 @@
+package uaa.tif;
+
+//import views.login;
+
+import views.Login;
+
+
+
+
+
+
+public class Tif {
+
+    public static void main(String[] args) {
+
+       Login i = new Login();
+       
+       i.setVisible(true);
+       i.setLocationRelativeTo(null);
+       
+    }
+}
